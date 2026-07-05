6b8342ebd47459dc5514933e0059568246a931a7
LigronAir <marsacher@hotmail.com>
2026-07-05 21:09:10 +0200

    refact con cors

--- src/api/register.js
+++ src/api/register.js
@@ -1,13 +1,21 @@
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "https://ligronair.tv",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type"
+};
+
 export async function register(request, env) {
 
     const body = await request.json();
 
-    return Response.json({
-
-        success: true,
-
-        recibido: body
-
-    });
+    return Response.json(
+        {
+            success: true,
+            recibido: body
+        },
+        {
+            headers: corsHeaders
+        }
+    );
 
 }
